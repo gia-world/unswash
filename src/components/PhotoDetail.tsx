8b07937a5f9172799fd378d6d3f3c0f8f3d4c6b1
Gia <gia.jihye.j@gmail.com>
2024-02-01 00:44:41 +0900

    Update: Query Templete props, loading

--- src/components/PhotoDetail.tsx
+++ src/components/PhotoDetail.tsx
@@ -27,7 +27,7 @@ export default function PhotoDetail({ photoId }: Props) {
   } = queryData.data!;
 
   return (
-    <QueryTemplete {...queryData}>
+    <QueryTemplete queryResult={queryData}>
       {queryData.data ? (
         <div className="flex flex-col h-full">
           <div className="flex justify-between p-4 ml-8">
